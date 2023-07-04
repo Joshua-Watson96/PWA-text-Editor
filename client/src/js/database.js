d8ab805b28bcf3ea85a03120ba9bb50635cc92e2
josh Watson <joshjkwatson@gmail.com>
2023-07-04 12:43:25 +1000

    added code to install/database js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,44 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Added logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // Create connection to DB
+  const jateDb = await openDB("jate", 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // create new transaction to DB
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Open up the desired objectStore
+  const store = tx.objectStore("jate");
+
+  // use PUT to update database data
+  const request = store.put({id: 1, value: content});
+
+  // confirm request  
+  const result = await request;
+  // log added data
+  console.log("Data saved to DB", result);
+};
+
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  // create connection to DB
+  const jateDb = await openDB("jate", 1);
+
+  // create new transaction to DB
+  const tx = jateDb.transaction("jate", "readonly");
+  
+  // Open up desired objectStore
+  const store = tx.objectStore("jate");
+  
+  // use .GET to get entry from the database
+  const request = store.get(1);
+  // Confirm request
+  const result = await request;
+  // log data retrieved from DB
+  console.log("data retrieved from DB",result.value);
+  return result.value
+}
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,31 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    //Store the event
+    window.defferedPrompt = event;
+    //Show the button
+    butInstall.classList.toggle("hidden", false)
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
+    if(!promptEvent){
+        return;
+    }
+    // show the prompt
+    promptEvent.prompt();
+
+    // reset the deffered prompt to null
+    window.defferedPrompt = null;
+    
+    // set the button to hidden
+    butInstall.classList.toggle("hidden", true)
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // clear prompt
+    window.defferedPrompt = null;
+});
